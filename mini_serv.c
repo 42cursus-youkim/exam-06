d2d7747520ebbcf9c6761cfc536f635aead4d098
scarf <greenscarf005@gmail.com>
2022-09-13 02:21:18 +0900

    docs: add even more comments

--- mini_serv.c
+++ mini_serv.c
@@ -55,6 +55,7 @@ void send_all_except(int fd, char* msg) {
   t_client* curs = g_clients;
 
   while (curs) {
+    // 쓰기 설정된 소켓에만 골라서 보냄
     if (curs->fd != fd && FD_ISSET(curs->fd, &fd_write)) {
       if (send(curs->fd, msg, strlen(msg), 0) == -1)
         fatal();
@@ -76,12 +77,12 @@ int insert_client(int fd) {
     fatal();
   *new = cons;
 
-  if (!g_clients) {
+  if (!g_clients) {  // 리스트가 비어있으면 첫 원소로
     g_clients = new;
   } else {
-    while (curs->next)
+    while (curs->next)  // 마지막 원소 찾기
       curs = curs->next;
-    curs->next = new;
+    curs->next = new;  // 마지막 원소 다음에 추가
   }
   return new->id;
 }
@@ -90,10 +91,11 @@ int delete_client(int fd) {
   t_client *del, *curs = g_clients;
   const int id = get_id(fd);
 
-  if (curs && curs->fd == fd) {
+  if (curs && curs->fd == fd) {  // 첫 원소일때
     g_clients = curs->next;
     free(curs);
   } else {
+    // 삭제할 원소 찾기
     while (curs && curs->next && curs->next->fd != fd)
       curs = curs->next;
     del = curs->next;
@@ -103,26 +105,33 @@ int delete_client(int fd) {
   return id;
 }
 
+/** @brief 연결 수락 후 curr_sock에 추가, 접속 메시지 송신 */
 void accept_connection(void) {
+  // 주소 초기화
   struct sockaddr_in clientaddr;
   socklen_t len = sizeof(clientaddr);
   int client_fd;
 
+  // 연결을 수락해 만들어진 소켓을 client_fd에 바인딩
   if ((client_fd = accept(sock_fd, (struct sockaddr*)&clientaddr, &len)) < 0)
     fatal();
 
+  // 버퍼에 환영 메시지 작성 후 송신
   memset(&buf, 0, 64);
   sprintf(buf, "server: client %d just arrived\n", insert_client(client_fd));
   send_all_except(client_fd, buf);
 
+  // 새로운 클라이언트의 소켓을 curr_sock 목록에 추가
   FD_SET(client_fd, &curr_sock);
 }
 
 void close_connection(int fd) {
+  // 버퍼에 퇴장 메시지 작성 후 송신
   memset(&buf, 0, 64);
   sprintf(buf, "server: client %d just left\n", delete_client(fd));
   send_all_except(fd, buf);
 
+  // 소켓을 curr_sock 목록에서 제거 후 접속 종료
   FD_CLR(fd, &curr_sock);
   close(fd);
 }
@@ -167,12 +176,13 @@ int main(int ac, char* av[]) {
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
   addr.sin_port = htons(atoi(av[1]));
 
-  // 소켓 생성 -> 소켓 fd에 주소 바인드 -> 소켓에서 듣기
+  // 소켓 생성 -> 소켓 fd에 주소 연결 -> 소켓에서 듣기
   if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0
       || bind(sock_fd, (const struct sockaddr*)&addr, sizeof(addr)) < 0
       || listen(sock_fd, 0) < 0)
     fatal();
 
+  // fd_set 초기화
   FD_ZERO(&curr_sock);          // 초기화
   FD_SET(sock_fd, &curr_sock);  // 소켓 fd 추가
 
@@ -183,7 +193,7 @@ int main(int ac, char* av[]) {
 
   // 메인 루프
   while (true) {
-    // 듣기/쓰기 소켓 목록 초기화
+    // 듣기/쓰기 소켓 변동 목록 초기화
     fd_write = fd_read = curr_sock;
     // 들어온 이벤트가 생길 때까지 대기
     if (select(get_max_fd() + 1, &fd_read, &fd_write, NULL, NULL) < 0)
@@ -198,8 +208,8 @@ int main(int ac, char* av[]) {
           if (len > 0) {
             broadcast_msg(fd);  // 다른 사용자에게 전부 송신
           } else {
-            close_connection(fd); // 연결이 끊겼으니 사용자 제거
-            break;  // 연결 목록이 변했으니 새로고침
+            close_connection(fd);  // 연결이 끊겼으니 사용자 제거
+            break;                 // 연결 목록이 변했으니 새로고침
           }
         }
       }
